70790c885ef26246a7116adcfe5b7348dad02a3a
danielCadenaZzz <dacadena@uniboyaca.edu.co>
2024-04-12 17:10:55 -0500

    se agrego contenido css y arreglos al js

--- gest_tareas/js/Tr.js
+++ gest_tareas/js/Tr.js
@@ -25,7 +25,7 @@ addBtn.addEventListener("click", (e) => {
 function addDeleteBtn() {
   const deleteBtn = document.createElement("button");
 
-  deleteBtn.textContent = "X";
+  deleteBtn.textContent = "eliminar";
   deleteBtn.className = "btn-delete";
 
   deleteBtn.addEventListener("click", (e) => {
@@ -41,3 +41,4 @@ function addDeleteBtn() {
 
   return deleteBtn;
 }
+
